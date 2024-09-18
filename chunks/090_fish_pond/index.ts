67dd90713d2fa448ac2624507ae5f156c5b4fefe
tss-legal-code <tss.legal.code@gmail.com>
2024-09-18 19:54:44 +0300

    add a simple pond with fishes

--- chunks/090_fish_pond/index.ts
+++ chunks/090_fish_pond/index.ts
@@ -1,5 +1,5 @@
 import { generateRandomColor as grc } from '../../shared/utils';
-import { Application, Assets } from 'pixi.js';
+import { Application, Assets, Container, Sprite } from 'pixi.js';
 
 function getApp() {
   const app = new Application({ background: grc(), resizeTo: window });
@@ -22,14 +22,87 @@ async function preloadAssets() {
   await Assets.load(assets);
 }
 
-async function main() {
-  await preloadAssets();
-  const app = getApp();
+function addBackground(app) {
+  const background = Sprite.from('background');
+  background.anchor.set(0.5);
+
+  background.height = app.screen.width > app.screen.height
+    ? app.screen.width
+    : app.screen.height;
+  background.scale.x = background.scale.y;
+
+  background.x = app.screen.width / 2;
+  background.y = app.screen.height / 2;
+
+  app.stage.addChild(background);
 }
 
+type Fish = Sprite & {
+  direction: number;
+  speed: number;
+  turnSpeed: number;
+};
 
+function getFishes(app) {
+  const container = new Container();
+  app.stage.addChild(container);
 
+  const fishes: Fish[] = [];
+  const count = 20;
+  const skins = ['fish1', 'fish2', 'fish3', 'fish4', 'fish5'];
 
+  for (let i = 0; i < count; i++) {
+    const skin = skins[i % skins.length];
+    const fish = Sprite.from(skin) as Fish;
+    fish.anchor.set(0.5);
+
+    fish.direction = Math.random() * Math.PI * 2;
+    fish.speed = 2 + Math.random() * 2;
+    fish.turnSpeed = Math.random() - 0.8;
+
+    fish.x = Math.random() * app.screen.width;
+    fish.y = Math.random() * app.screen.height;
+    fish.scale.set(0.5 + Math.random() * 0.2);
+
+    container.addChild(fish);
+    fishes.push(fish);
+  }
+
+  return fishes;
+};
+
+function animateFishes(app, fishes, delta) {
+  const padding = 100;
+  const width = app.screen.width + padding * 2;
+  const height = app.screen.height + padding * 2;
+
+  fishes.forEach(fish => {
+    fish.direction += fish.turnSpeed * 0.01 * delta;
+    fish.x += Math.sin(fish.direction) * fish.speed * delta;
+    fish.y += Math.cos(fish.direction) * fish.speed * delta;
+    fish.rotation = -fish.direction - Math.PI / 2;
+    if (fish.x < -padding) {
+      fish.x += width;
+    } else if (fish.x > app.screen.width + padding) {
+      fish.x -= width;
+    }
+    if (fish.y < -padding) {
+      fish.y += height;
+    } else if (fish.y > app.screen.height + padding) {
+      fish.y -= height;
+    }
+  });
+};
+
+async function main() {
+  const app = getApp();
+  await preloadAssets();
+  addBackground(app);
+  const fishes = getFishes(app);
+  app.ticker.add((delta) => {
+    animateFishes(app, fishes, delta);
+  });
+}
 
 main();
 console.log('Hello from 090_fish_pond');
